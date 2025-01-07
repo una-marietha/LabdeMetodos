dece89b1151b4cb51157098a9cd436f69ded107d
Marietha Pérez <marietha.perez@gmail.com>
2025-01-07 15:49:53 -0600

    Paleta para ggplot

--- R/colores.R
+++ R/colores.R
@@ -20,4 +20,16 @@ obtener_paleta <- function() {
   return(paleta)
 }
 
+#' Paleta de colores personalizada para ggplot
+#'
+
+#'
+#' @return scale_color_manual para usar con ggplot2.
+#' @export
+scale_labdem_palette <- function() {
+  paleta <- c( "#75215F","#842045", "#213951","#4B1B56", "#58595B","#091925", "#2A3069",
+                "#010C1C") # Ocho colores
+
+  ggplot2::scale_color_manual(values = paleta)
+}
 
