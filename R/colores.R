704354d44b7607eee9ab8e6e3fd4cb95ebf588dd
Marietha Pérez <marietha.perez@gmail.com>
2025-01-08 11:31:34 -0600

    Reordenamiento en paleta de colores

--- R/colores.R
+++ R/colores.R
@@ -27,7 +27,7 @@ obtener_paleta <- function() {
 #' @return scale_color_manual para usar con ggplot2.
 #' @export
 scale_labdem_palette <- function() {
-  paleta <- c( "#75215F","#842045", "#213951","#4B1B56", "#58595B","#091925", "#2A3069",
+  paleta <- c( "#75215F","#091925","#842045", "#213951","#4B1B56", "#58595B", "#2A3069",
                 "#010C1C") # Ocho colores
 
   ggplot2::scale_color_manual(values = paleta)
